9dba8174a073a2cbd83e5dada91b31e55aa941cb
PierfrancescoChiucchiolo <pierfrancescochiucchiolo@gmail.com>
2024-11-12 14:22:16 +0100

    upload dei database nella cartella

--- Lezione 11/Lezione 11 Note.py
+++ Lezione 11/Lezione 11 Note.py
@@ -1,4 +1,8 @@
 '''
-ripasso di numpy, installazione di scipy, cominciamo con sql tramite mamp
+ripasso di numpy, installazione di scipy, cominciamo con sql tramite mamp e xampp
 
+-- commento singola linea
+/*
+commento multilinea
+*/
 '''
