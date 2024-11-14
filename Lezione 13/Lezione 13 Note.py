79249aba52f3ea6239a2a1635e5d2d8b28ce899b
PierfrancescoChiucchiolo <pierfrancescochiucchiolo@gmail.com>
2024-11-14 15:29:06 +0100

    inizio json e requests

---
+++ Lezione 13/Lezione 13 Note.py
@@ -0,0 +1,71 @@
+'''
+
+consegna della modifica del file di ieri
+
+all'avvio del programma si crea un utente amministratore
+nel menù ci sarà la possibilità di aggiungere gli utenti
+ogni utente vede solo i suoi voti e medie, solo l'amministratore può aggiungere voti e studenti e visualizzare tutti gli studenti
+
+md5 algoritmo di criptazione
+import hashlib
+
+result = hashlib.md5("ciao".encode.hexdigest())
+si confronta se l'input criptato sia uguale al dato criptato sul db
+
+
+
+
+SELECT CASE WHEN ( (SELECT isadmin from users) not false )
+    THEN /// query di update ///
+    ELSE select 1 from dual where false
+END
+
+
+query con return set vuoto
+select 1
+from dual
+where false
+
+
+## costruisci la query python per stampare i suoi voti da studente
+
+query = "select * from voti join studenti on voti.id = studenti.id where studenti.id in select users.id from studenti join users on studenti.id = users.id where studenti.name = " + nome
+
+
+## rendi admin un utente
+
+query = "update users set isadmin where user.id = select users.id from users join studenti on users.id = studenti.id where users.username = " + username
+
+
+#stampa user
+
+query = "select users.id, users.name, users.isadmin from users"
+
+
+## inserisci in studenti admin admin
+
+
+
+'''
+
+
+
+import json
+
+## vuole per forza ' esterno ' e " interno "
+filejson = '{"chiave1" : "valore1", "chiave2" : "valore2"}'
+filejsonconvertito = json.loads(filejson)
+fileestratto = json.dumps(filejsonconvertito, indent = 4)
+
+
+##API servono per far communicare diversi servizi e linguaggi tra di loro
+
+link1 = "https://open-meteo.com"
+link2 = "https://web.postman.co/"
+
+## pip install requests
+import requests
+
+response = requests.get("www.google.it")
+
+
