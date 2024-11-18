56d2ee1c50b28c102155c01c30c831db5a1866dc
PierfrancescoChiucchiolo <pierfrancescochiucchiolo@gmail.com>
2024-11-18 09:31:19 +0100

    inizio con pandas e jupiter

---
+++ Lezione 15/Lezione 15 Note.py
@@ -0,0 +1,12 @@
+'''
+
+pandas è una libreria orientata alle tables come in sql, ma direttamente in python
+introduce dataframe (come fogli di calcolo e table sql) e serie
+
+usiamo un foglio jupiter, si creano in estensione .ipynb
+
+
+
+'''
+
+import pandas as pd
